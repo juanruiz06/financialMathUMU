12c3649f2d83bcca1a71efd408751a4f00758e90
2 <JJ@MacBook-Pro-de-1.local>
2026-02-17 17:40:02 +0100

    Fix TypeError en Home y ajuste de rutas

--- Home.py
+++ Home.py
@@ -4,8 +4,8 @@ import streamlit as st
 st.set_page_config(page_title="Inicio", layout="wide")
 st.markdown("---")
 st.subheader("Acceso a Simuladores")
-col1= st.columns(1)
+col1, = st.columns(1)
 
 with col1:
     if st.button("Pricing"):
-        st.switch_page("pages/pricing.py")
+        st.switch_page("pages/Pricing.py")
